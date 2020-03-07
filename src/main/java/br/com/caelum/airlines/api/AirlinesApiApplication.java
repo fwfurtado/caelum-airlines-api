a111123579efeec53ccf4e21b55bed367e0ca4ff
Fernando <fwfurtado@gmail.com>
2020-03-07 19:04:16 -0300

    Create an empty Spring Boot project

---
+++ src/main/java/br/com/caelum/airlines/api/AirlinesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.caelum.airlines.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirlinesApiApplication.class, args);
+    }
+
+}
